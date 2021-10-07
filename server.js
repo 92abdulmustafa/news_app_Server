7746cb624db754ee0a5d0c023af3af1be34de277
Shariq Ahmed <shariq.ahmed525@gmail.com>
2021-10-07 05:42:51 +0500

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const globalHelpers = require("./utils/globalhelpers");
+const globalHelpers = require("./utils/globalHelpers");
 
 const app = express();
 // Create Express app
--- utils/globalHelpers.js
+++ utils/globalHelpers.js
@@ -1,5 +1,3 @@
-const mongoose = require("mongoose");
-
 const globalHelpers = {};
 
 globalHelpers.handleMongooseError = function (response) {
@@ -25,59 +23,4 @@ globalHelpers.handleMongooseError = function (response) {
 
   return returnResponse;
 };
-
-globalHelpers.isNullOrUndefined = function (value) {
-  if (typeof value === "undefined" || value === null || value === "") {
-    return true;
-  }
-};
-
-globalHelpers.removedUndefinedValues = function (obj) {
-  const payload = {};
-
-  for (const key in obj) {
-    if (!globalHelpers.isNullOrUndefined(obj[`${key}`])) {
-      payload[`${key}`] = obj[`${key}`];
-    }
-  }
-
-  return payload;
-};
-
-globalHelpers.getUserPublicProfile = function (user, removeDeleteKeys = true) {
-  if (user.toJSON) {
-    user = user.toJSON();
-  }
-
-  delete user.googleId;
-  delete user.deviceToken;
-  delete user.isNeedToUpdate;
-  if (removeDeleteKeys) {
-    delete user.deleted;
-    delete user.blocked;
-  }
-  return user;
-};
-
-globalHelpers.skipDocCalculate = (page) => {
-  return (Number(page) - 1) * process.env.LIMIT;
-};
-
-globalHelpers.calculateTotalPage = function (totalDocs, limit) {
-  if (totalDocs === 0 || limit === 0) {
-    return 1;
-  }
-  return Math.ceil(totalDocs / limit);
-};
-
-globalHelpers.isValidObjectId = function (id) {
-  return mongoose.Types.ObjectId.isValid(id);
-};
-
-globalHelpers.getTomorrowsMillisecond = function () {
-  var currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() + 30);
-  return currentDate.getTime();
-};
-
 module.exports = globalHelpers;
